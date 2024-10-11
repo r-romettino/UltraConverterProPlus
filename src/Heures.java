58f694e030c7b015b10ff1c1feaf4bd5daa9e9d0
r-romettino <125373638+r-romettino@users.noreply.github.com>
2024-10-11 15:30:00 +0200

    Ajout unités de temps (#1)

---
+++ src/Heures.java
@@ -0,0 +1,15 @@
+public class Heures implements IUnite {
+    @Override
+    public String toString() {
+        return "heures";
+    }
+    @Override
+    public float convertToIS(float hours) {
+        return 60 * 60 * hours;
+    }
+
+    @Override
+    public float convertFromIS(float seconds) {
+        return seconds / (60 * 60);
+    }
+}
---
+++ src/Secondes.java
@@ -0,0 +1,15 @@
+public class Secondes implements IUnite {
+    @Override
+    public String toString() {
+        return "secondes";
+    }
+    @Override
+    public float convertToIS(float seconds) {
+        return seconds;
+    }
+
+    @Override
+    public float convertFromIS(float seconds) {
+        return seconds;
+    }
+}
